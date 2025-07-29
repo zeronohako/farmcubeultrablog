3e338f67f2cc58b8a9533ec3234caac822b3992c
yunsuhan0107 <46840483+yunsuhan0107@users.noreply.github.com>
2025-07-29 17:30:47 +0900

    change design colorscheme

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Farm Cube Ultra Blog',
   author: 'Brian Ji',
   headerTitle: 'Farm Cube Ultra Blog',
-  description: 'Logs of growing lettuces utilizing Farm Cube Ultra',
+  description: 'Logs of growing lettuces with the Farm Cube Ultra',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
